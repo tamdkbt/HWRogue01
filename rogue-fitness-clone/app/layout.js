35cef4830e4781ef382b8580292d07fdcbb1f346
tamdkbt <125808063+tamdkbt@users.noreply.github.com>
2024-12-06 20:02:31 +0700

    Update package dependencies to latest versions, refactor RootLayout to include Providers for NextUI integration, and add Providers component for context management.

--- rogue-fitness-clone/app/layout.js
+++ rogue-fitness-clone/app/layout.js
@@ -3,17 +3,20 @@ import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import CanvasCursor from "@/components/CanvasCursor";
+import { Providers } from "./providers"
 
 const inter = Inter({ subsets: ['latin', 'vietnamese'] })
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="vi">
-      <body className={inter.className}>
-        <Header />
-        <CanvasCursor />
-        <main>{children}</main>
-        <Footer />
+    <html lang="en">
+      <body>
+        <Providers>
+          <Header />
+          <CanvasCursor />
+          <main>{children}</main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   )
